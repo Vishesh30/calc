2a4ce7f3c4cf6de05e2068d00d5f49e2470672df
Agrawal <vishesh.agrawal@sap.com>
2019-11-05 11:38:54 +0530

    updating package name

--- packages/sol/index.js
+++ packages/sol/index.js
@@ -1,5 +1,5 @@
-var add = require('addmod');
-var subtract = require('submod');
+var add = require('vishesh_addmod');
+var subtract = require('vishesh_submod');
  
 var sum = add.add(2,3);
 var diff = subtract.subtract(3,2);
