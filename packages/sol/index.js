b38704f8f13c9d6f20e55e5f16e4b01af9658393
Agrawal <vishesh.agrawal@sap.com>
2019-11-05 12:25:31 +0530

    expoted a module from sol package

--- packages/sol/index.js
+++ packages/sol/index.js
@@ -1,7 +1,12 @@
 var add = require('vishesh_addmod');
 var subtract = require('vishesh_submod');
  
-var sum = add.add(2,3);
-var diff = subtract.subtract(3,2);
+module.exports.addsub = function(x,y){
+    var sum = add.add(2,3);
+    var diff = subtract.subtract(3,2);
+
+    return "result of addition is " + sum + "result of sub is " + diff;
+}
+
  
 console.log("Sum: " + sum + " Diff: " + diff);
