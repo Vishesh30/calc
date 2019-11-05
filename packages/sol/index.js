b0831b55a2e98225efd65a46f76295e8de1515b8
Agrawal <vishesh.agrawal@sap.com>
2019-11-05 10:36:22 +0530

    Initial repo for testing

---
+++ packages/sol/index.js
@@ -0,0 +1,7 @@
+var add = require('additionmodule');
+var subtract = require('subtractionmodule');
+ 
+var sum = add.add(2,3);
+var diff = subtract.subtract(3,2);
+ 
+console.log("Sum: " + sum + " Diff: " + diff);
