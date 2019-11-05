7894daf5f001310622d5a82d54bd3392efab1524
Agrawal <vishesh.agrawal@sap.com>
2019-11-05 10:41:39 +0530

    changed package name

--- packages/sol/index.js
+++ packages/sol/index.js
@@ -1,5 +1,5 @@
-var add = require('additionmodule');
-var subtract = require('subtractionmodule');
+var add = require('addmod');
+var subtract = require('submod');
  
 var sum = add.add(2,3);
 var diff = subtract.subtract(3,2);
