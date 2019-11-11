1dfb88229be4bdd32de3f2ea925a64dbf65f648b
Agrawal <vishesh.agrawal@sap.com>
2019-11-11 10:23:16 +0530

    Changes

---
+++ packages/sol/node_modules/vishesh_addmod/Formatter/index.js
@@ -0,0 +1,5 @@
+module.exports.outputformat = function(result){
+    console.log("-----------------------------------------");
+    console.log("The result is " + result);
+    console.log("-----------------------------------------");
+}
